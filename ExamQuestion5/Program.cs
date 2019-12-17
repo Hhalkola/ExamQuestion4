80a8ced60c00fc9b697f31a450fd19279d571403
hhalkola <herman.halkola@gmail.com>
2019-12-17 23:28:13 +0200

    Add project files.

---
+++ ExamQuestion5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExamQuestion4
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            //A) Tee luokka Tuotantotapahtuma, jossa on jäsenmuuttujat (propertyt KoneId, KoneenNimi, TuoteId, KoneAika. 
+            //Lisää luokkaan konstruktori ja ToString() metodi
+            //B)Tee ohjelma, joka lukee tiedoston koneajaat.txt ja lisää tuotantotapahtuma-oliot listaan. Tulosta lista näytölle.
+            //C)Laske kuinka kauan koneita on käytetty kunkin tuotteen (101, 102, 103) valmistamiseen. Käytä apuna Dictionarya.
+            //Dictionaryssa avaimeksi tulee tuotteen tunnus. Arvoksi tulee tuotteen valmistukseen tarvittujen koneiden toiminta-aika yhteensä
+        {
+            //Tehdään lista
+            List<Tuotantotapahtuma> tuotantotapahtumat = new List<Tuotantotapahtuma>();
+
+            //Tehdään dictionary
+            Dictionary<int, int> valmistusAjat = new Dictionary<int, int>();
+
+            //Tiedostosta lukeminen
+            string polku = (@"C:\Users\Herman\source\repos\ExamQuestion5\ExamQuestion5\koneajat.txt");
+
+            if (File.Exists(polku))
+            {
+                StreamReader streamReader = File.OpenText(polku);
+
+                string row;
+                while ((row = streamReader.ReadLine()) != null)
+                {
+                    string[] taulukko;
+                    taulukko = row.Split(';');
+                    int koneid = int.Parse(taulukko[0]);
+                    string koneennimi = taulukko[1];
+                    int tuoteid = int.Parse(taulukko[2]);
+                    int koneaika = int.Parse(taulukko[3]);
+                    //Luodaan uusi olio joka lisätään listaan
+                    tuotantotapahtumat.Add(new Tuotantotapahtuma(koneid, koneennimi, tuoteid, koneaika));
+                }
+            }
+            //Tulostetaan listan itemit/oliot
+            foreach (var item in tuotantotapahtumat)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExamQuestion5/Tuotantotapahtuma.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamQuestion4
+{
+    class Tuotantotapahtuma
+    {
+        public int KoneID { get; set; }
+        public string KoneenNimi { get; set; }
+        public int TuoteID { get; set; }
+        public int KoneAika { get; set; }
+        public Tuotantotapahtuma(int koneId, string koneenNimi, int tuoteId, int koneAika)
+        {
+            this.KoneID = koneId;
+            this.KoneenNimi = koneenNimi;
+            this.TuoteID = tuoteId;
+            this.KoneAika = koneAika;
+        }
+
+        public override string ToString()
+        {
+            return KoneID + " " + KoneenNimi + " " + TuoteID + " " + KoneAika;
+        }
+    }
+}
